2e9cff323705836c88537228cff2dd2ec5936ba2
Kyo-XZ <ms.noctis@gmail.com>
2026-02-12 18:21:29 +0100

    debut travail classe character

--- Model/Combat/Character.cs
+++ Model/Combat/Character.cs
@@ -8,5 +8,16 @@ namespace ProjetPOO.Model.Combat
 {
     public class Character
     {
+        private string _name;
+        private int _maxHp;
+        private int _currentHp;
+        private int _attack;
+        private int _defense;
+        private int _agility;
+
+        public bool IsAlive()
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
