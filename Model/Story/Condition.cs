633700815ac872d603b9ada2aed498a8431b967f
Maximilien Schaekels <ms.noctis@gmail.com>
2026-02-23 16:02:55 +0100

    Structuration de la classe Condition et alignement avec ConditionType

--- Model/Story/Condition.cs
+++ Model/Story/Condition.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ProjetPOO.Model.Game;
+using ProjetPOO.Model.Story.Enums;
+using ProjetPOO.Utilities.EntriesValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,54 @@ namespace ProjetPOO.Model.Story
 {
     public class Condition
     {
+        private int _id;
+        private ConditionType _type;
+        private int? _minValue;
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                if (ValidUtils.CheckIfPositiveNumber(value))
+                    _id = value;
+            }
+        }
+
+        public ConditionType Type
+        {
+            get => _type;
+            set => _type = value;
+        }
+
+
+        public int? MinValue
+        {
+            get => _minValue;
+            set
+            {
+                if (value == null || ValidUtils.CheckIfNonNegativeNumber(value.Value))
+                    _minValue = value;
+            }
+        }
+
+        public Condition(int id, ConditionType type, int? minValue = null)
+        {
+            Id = id;
+            Type = type;
+            MinValue = minValue;
+        }
+
+        public bool Evaluate(GameState state)
+        {
+            throw new NotImplementedException();
+
+        }
+
+        public bool Validate(out List<string> errors)
+        {
+            throw new System.NotImplementedException();
+        }
+
     }
 }
