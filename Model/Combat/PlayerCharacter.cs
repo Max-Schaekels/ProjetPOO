5cf44e6b378149859e43f70bb7c4f069b65fa58b
Maximilien Schaekels <ms.noctis@gmail.com>
2026-02-23 20:09:41 +0100

    Structuration de PlayerCharacter (gestion Level, Experience et préparation du système de level up)

--- Model/Combat/PlayerCharacter.cs
+++ Model/Combat/PlayerCharacter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProjetPOO.Utilities.EntriesValidation;
 
 namespace ProjetPOO.Model.Combat
 {
@@ -11,6 +12,32 @@ namespace ProjetPOO.Model.Combat
         private int _experience;
         private int _level;
         
+        public int Experience
+        {
+            get => _experience;
+            private set
+            {
+                if (ValidUtils.CheckIfNonNegativeNumber(value))
+                    _experience = value;
+            }
+        }
+
+        public int Level
+        {
+            get => _level;
+            private set
+            {
+                if (ValidUtils.CheckIfPositiveNumber(value))
+                    _level = value;
+            }
+        }
+
+        public PlayerCharacter(string name, int maxHp, int attackPower, int defense, int agility, int experience = 0, int level = 1)
+            : base(name, maxHp, attackPower, defense, agility)
+        {
+            Experience = experience;
+            Level = level;
+        }
 
         public void GainExperience(int amount)
         {
@@ -22,9 +49,14 @@ namespace ProjetPOO.Model.Combat
             throw new System.NotImplementedException();
         }
 
-        public void LevelUp()
+        private void LevelUp()
         {
             throw new System.NotImplementedException();
+        }
+
+        private int GetExperienceRequiredForLevel(int level)
+        {
+            throw new NotImplementedException();
         }
     }
    
