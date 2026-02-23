8dd75d6f38ffa12ab677627aa5ac9882eeba6c2d
Maximilien Schaekels <ms.noctis@gmail.com>
2026-02-23 16:13:34 +0100

    Structuration de la classe Effect et alignement avec EffectType

--- Model/Story/Effect.cs
+++ Model/Story/Effect.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ProjetPOO.Model.Game;
+using ProjetPOO.Model.Story.Enums;
+using ProjetPOO.Utilities.EntriesValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,66 @@ namespace ProjetPOO.Model.Story
 {
     public class Effect
     {
+        private const int MINIMUM_FLAG_KEY_LENGTH = 3;
+        private const int MAXIMUM_FLAG_KEY_LENGTH = 100;
+        private int _id;
+        private EffectType _type;
+        private int? _amount;
+        private string? _flagKey;
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                if (ValidUtils.CheckIfPositiveNumber(value))
+                    _id = value;
+            }
+        }
+
+        public EffectType Type
+        {
+            get => _type;
+            set => _type = value;
+        }
+
+        public int? Amount
+        {
+            get => _amount;
+            set
+            {
+                if (value == null || ValidUtils.CheckIfNonNegativeNumber(value.Value))
+                    _amount = value;
+            }
+        }
+
+        public string? FlagKey
+        {
+            get => _flagKey;
+            set
+            {
+                if (value == null || ValidUtils.CheckEntryName(value, MINIMUM_FLAG_KEY_LENGTH, MAXIMUM_FLAG_KEY_LENGTH))
+                    _flagKey = value;
+            }
+        }
+
+        public Effect(int id, EffectType type, int? amount = null, string? flagKey = null)
+        {
+            Id = id;
+            Type = type;
+            Amount = amount;
+            FlagKey = flagKey;
+        }
+
+        public void Apply(GameState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Validate(out List<string> errors)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
