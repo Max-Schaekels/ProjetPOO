2a71dbe8e37336c82be09f69ebd1ea4cec267e17
Maximilien Schaekels <ms.noctis@gmail.com>
2026-02-24 15:48:25 +0100

    Finalisation de la classe Loot (objet non persistant)

--- Model/Game/Loot.cs
+++ Model/Game/Loot.cs
@@ -3,22 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProjetPOO.Utilities.EntriesValidation;
 
 namespace ProjetPOO.Model.Game
 {
     public class Loot
     {
-        private int _id;
+        
         private int _goldAmount;
         private int _potionAmount;
         private int _keysAmount;
 
-        public Loot(int id, int goldAmount, int potionAmount, int keysAmount)
+        
+
+        public int GoldAmount
         {
-            _id = id;
-            _goldAmount = goldAmount;
-            _potionAmount = potionAmount;
-            _keysAmount = keysAmount;
+            get => _goldAmount;
+            set
+            {
+                if (ValidUtils.CheckIfNonNegativeNumber(value))
+                    _goldAmount = value;
+            }
+        }
+
+        public int PotionAmount
+        {
+            get => _potionAmount;
+            set
+            {
+                if (ValidUtils.CheckIfNonNegativeNumber(value))
+                    _potionAmount = value;
+            }
+        }
+
+        public int KeysAmount
+        {
+            get => _keysAmount;
+            set
+            {
+                if (ValidUtils.CheckIfNonNegativeNumber(value))
+                    _keysAmount = value;
+            }
+        }
+
+        public Loot( int goldAmount, int potionAmount, int keysAmount)
+        {
+            GoldAmount = goldAmount;
+            PotionAmount = potionAmount;
+            KeysAmount = keysAmount;
         }
 
         public void Apply(GameState state)
