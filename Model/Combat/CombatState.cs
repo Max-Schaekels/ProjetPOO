4077faa25113baa87dac1dc999328bbb7ebf3fdb
Maximilien Schaekels <ms.noctis@gmail.com>
2026-02-24 23:25:24 +0100

    Refactorisation de CombatState (constructeur simplifié et état initial cohérent)

--- Model/Combat/CombatState.cs
+++ Model/Combat/CombatState.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ProjetPOO.Model.Combat.Enums;
+using ProjetPOO.Model.Game;
+using ProjetPOO.Utilities.EntriesValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,90 @@ namespace ProjetPOO.Model.Combat
 {
     public class CombatState
     {
+        private PlayerCharacter _player;
+        private Enemy _enemy;
+        private bool _isPlayerTurn;
+        private int _turnCount;
+        private bool _isDefendingThisTurn;
+        private CombatResult _result;
+
+        public PlayerCharacter Player
+        {
+            get => _player;
+            set
+            {
+                if(ValidUtils.CheckIfNotNull(value))
+                    _player = value;
+            }
+        }
+
+        public Enemy Enemy
+        {
+            get => _enemy;
+            set
+            {
+                if (ValidUtils.CheckIfNotNull(value))
+                    _enemy = value;
+            }
+        }
+
+        public bool IsPlayerTurn
+        {
+            get => _isPlayerTurn;
+            set => _isPlayerTurn = value;
+        }
+
+        public int TurnCount
+        {
+            get => _turnCount;
+            set
+            {
+                if (ValidUtils.CheckIfNonNegativeNumber(value))
+                    _turnCount = value;
+            }
+        }
+
+        public bool IsDefendingThisTurn
+        {
+            get => _isDefendingThisTurn;
+            set => _isDefendingThisTurn = value;
+        }
+
+        public CombatResult Result { get; private set; }
+
+        public CombatState(PlayerCharacter player, Enemy enemy)
+        {
+            Player = player;
+            Enemy = enemy;
+
+            
+            IsPlayerTurn = true;                 
+            TurnCount = 0;                       
+            IsDefendingThisTurn = false;         
+            Result = CombatResult.InProgress;    
+        }
+
+
+        public void ResolvePlayerAction(CombatActionType action, GameState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ResolveEnemyAction(GameState state)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CheckEnd()
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void ClearDefense()
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
