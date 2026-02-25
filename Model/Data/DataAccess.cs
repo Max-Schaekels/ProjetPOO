76b904ef32a6482da8066dd675ff833cd7511ed5
Maximilien Schaekels <ms.noctis@gmail.com>
2026-02-25 17:01:37 +0100

    Pose de la structure de DataAccess

--- Model/Data/DataAccess.cs
+++ Model/Data/DataAccess.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ProjetPOO.Model.Game;
+using ProjetPOO.Model.Story;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,46 @@ namespace ProjetPOO.Model.Data
 {
     public class DataAccess
     {
+        public void SaveScenario(Scenario scenario)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public void SaveGame(SaveGame save)
+        {
+            throw new NotImplementedException();
+        }
+
+        public SaveGame GetSaveGame(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Scenario> GetScenarios()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Scenario GetScenario(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<SaveGame> GetSaveGames()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteSaveGame(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteScenario(int id)
+        {
+            throw new NotImplementedException();
+        }
+
     }
 }
