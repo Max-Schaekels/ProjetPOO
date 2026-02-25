03ee0d209a132833e358ca66a9801aae6b033f15
Maximilien Schaekels <ms.noctis@gmail.com>
2026-02-25 16:26:00 +0100

    Ajout et structuration de la classe SaveGame (gestion des métadonnées et état du jeu)

--- Model/Game/SaveGame.cs
+++ Model/Game/SaveGame.cs
@@ -3,10 +3,86 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProjetPOO.Utilities.EntriesValidation;
 
 namespace ProjetPOO.Model.Game
 {
     public class SaveGame
     {
+        private const int MINIMUM_NAME_LENGTH = 3;
+        private const int MAXIMUM_NAME_LENGTH = 100;
+        private int _id;
+        private string _name;
+        private DateTime _createdAt;
+        private DateTime _lastSavedAt;
+        private GameState _state;
+
+        public int Id
+        {
+            get => _id;
+            set
+            {
+                if (ValidUtils.CheckIfPositiveNumber(value))
+                    _id = value;
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (ValidUtils.CheckEntryName(value, MINIMUM_NAME_LENGTH, MAXIMUM_NAME_LENGTH))
+                    _name = value;
+            }
+        }
+
+        public DateTime CreatedAt
+        {
+            get => _createdAt;
+            set => _createdAt = value;
+        }
+
+        public DateTime LastSavedAt
+        {
+            get => _lastSavedAt;
+            set => _lastSavedAt = value;
+        }
+
+
+
+        public GameState State
+        {
+            get => _state;
+            set
+            {
+                if (ValidUtils.CheckIfNotNull(value))
+                    _state = value;
+            }
+        }
+
+        public SaveGame(int id, string name, DateTime createdAt, DateTime lastSavedAt, GameState state)
+        {
+            Id = id;
+            Name = name;
+            CreatedAt = createdAt;
+            LastSavedAt = lastSavedAt;
+            State = state;
+        }
+
+        public SaveGame(string name, DateTime createdAt, DateTime lastSavedAt, GameState state)
+        {
+            Name = name;
+            CreatedAt = createdAt;
+            LastSavedAt = lastSavedAt;
+            State = state;
+        }
+
+        public void UpdateFrom(GameState state)
+        {
+            throw new NotImplementedException();
+        }
+
+
     }
 }
