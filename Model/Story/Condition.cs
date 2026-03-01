6a6587b871a9a266bbab28586b983a6ee0fdc6da
Maximilien Schaekels <ms.noctis@gmail.com>
2026-03-01 23:55:26 +0100

    feat(Story): implémentation Condition et Effect
    - Implémentation complète de Condition (Evaluate + Validate)
    - Implémentation complète de Effect (Apply + Validate)
    - Validation stricte des objets avant persistance
    - Effets tolérants pour les retraits (gold, potions, clés)
    - Ajout de ConsumeMany pour factoriser les suppressions multiples

--- Model/Story/Condition.cs
+++ Model/Story/Condition.cs
@@ -13,7 +13,7 @@ namespace ProjetPOO.Model.Story
     {
         private int _id;
         private ConditionType _type;
-        private int? _minValue;
+        private int _minValue;
 
         public int Id
         {
@@ -28,21 +28,27 @@ namespace ProjetPOO.Model.Story
         public ConditionType Type
         {
             get => _type;
-            set => _type = value;
+            private set => _type = value;
         }
 
 
-        public int? MinValue
+        public int MinValue
         {
             get => _minValue;
-            set
+            private set
             {
-                if (value == null || ValidUtils.CheckIfNonNegativeNumber(value.Value))
+                if ( ValidUtils.CheckIfPositiveNumber(value))
                     _minValue = value;
             }
         }
 
-        public Condition(int id, ConditionType type, int? minValue = null)
+        public Condition( ConditionType type, int minValue = 1)
+        {
+            Type = type;
+            MinValue = minValue;
+        }
+        
+        public Condition(int id, ConditionType type, int minValue = 1) 
         {
             Id = id;
             Type = type;
@@ -51,13 +57,68 @@ namespace ProjetPOO.Model.Story
 
         public bool Evaluate(GameState state)
         {
-            throw new NotImplementedException();
+            if(state == null)
+            {
+                throw new ArgumentNullException(nameof(state));
+            }
+                
+            if(state.PlayerInventory == null)
+            {
+                throw new InvalidOperationException("L'inventaire du joueur n'est pas initialisé.");
+            }
+
+            switch (Type)
+            {
+                case ConditionType.HasPotion:
+                    return state.PlayerInventory.PotionsCount >= MinValue;
+                case ConditionType.HasKey:
+                    return state.PlayerInventory.KeysCount >= MinValue;
+                case ConditionType.MinGold:
+                    return state.Gold >= MinValue;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(Type), "Type de condition inconnu.");
+            }
 
         }
 
         public bool Validate(out List<string> errors)
         {
-            throw new System.NotImplementedException();
+           errors = new List<string>();
+
+
+            if (!Enum.IsDefined(typeof(ConditionType), Type))
+            {
+                errors.Add("Type de condition invalide.");
+            }
+
+            if (!ValidUtils.CheckIfPositiveNumber(MinValue))
+            {
+                if(Type == ConditionType.HasPotion)
+                {
+                    errors.Add("MinValue doit être au moins 1 pour HasPotion.");
+                }
+                else if(Type == ConditionType.HasKey)
+                {
+                    errors.Add("MinValue doit être au moins 1 pour HasKey.");
+                }
+                else if(Type == ConditionType.MinGold)
+                {
+                    errors.Add("MinValue doit être au moins 1 pour MinGold.");
+                }
+            }
+
+            return errors.Count == 0;
+
+        }
+
+        public void ChangeMinValue(int newValue)
+        {
+            if (!ValidUtils.CheckIfPositiveNumber(newValue))
+            {
+                throw new ArgumentException("MinValue doit être un nombre positif (>= 1).", nameof(newValue));
+            }
+
+            MinValue = newValue;
         }
 
     }
--- Model/Story/Effect.cs
+++ Model/Story/Effect.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace ProjetPOO.Model.Story
 {
     public class Effect
@@ -62,14 +63,161 @@ namespace ProjetPOO.Model.Story
             FlagKey = flagKey;
         }
 
+        public Effect(EffectType type, int? amount = null, string? flagKey = null)
+        {
+            Type = type;
+            Amount = amount;
+            FlagKey = flagKey;
+        }
+
         public void Apply(GameState state)
         {
-            throw new NotImplementedException();
+            if (state == null)
+            {
+                throw new ArgumentNullException(nameof(state));
+            }
+
+            switch (Type)
+            {
+                case EffectType.AddGold:
+                    state.AddGold(Amount!.Value);
+                    break;
+
+                case EffectType.RemoveGold:
+                    int goldToRemove = Amount!.Value;
+
+                    if (goldToRemove >= state.Gold)
+                    {
+                        state.Gold = 0;
+                    }
+                    else
+                    {
+                        state.Gold = state.Gold - goldToRemove;
+                    }
+                    break;
+
+                case EffectType.Damage:
+                    state.TakeDamage(Amount!.Value);
+                    break;
+
+                case EffectType.AddPotion:
+                    state.PlayerInventory.AddPotion(Amount!.Value);
+                    break;
+
+                case EffectType.RemovePotion:
+                    ConsumeMany(Amount!.Value, state.PlayerInventory.ConsumePotion);
+                    break;
+
+                case EffectType.AddKey:
+                    state.PlayerInventory.AddKey(Amount!.Value);
+                    break;
+
+                case EffectType.RemoveKey:
+                    ConsumeMany(Amount!.Value, state.PlayerInventory.ConsumeKey);
+                    break;
+
+                case EffectType.SetFlag:
+                    state.SetFlag(FlagKey!);
+                    break;
+
+                case EffectType.RemoveFlag:
+                    state.RemoveFlag(FlagKey!);
+                    break;
+
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(Type), "Type d'effet invalide.");
+            }
         }
 
         public bool Validate(out List<string> errors)
         {
-            throw new NotImplementedException();
+            errors = new List<string>();
+
+
+            if (!Enum.IsDefined(typeof(EffectType), Type))
+            {
+                errors.Add("Type d'effet invalide.");
+            }
+
+            switch (Type)
+            {
+                case EffectType.AddGold:
+                    ValidateAmount(errors);
+                    break;
+                case EffectType.RemoveGold:
+                    ValidateAmount(errors);
+                    break;
+                case EffectType.Damage:
+                    ValidateAmount(errors);
+                    break;
+                case EffectType.AddPotion:
+                    ValidateAmount(errors);
+                    break;
+                case EffectType.RemovePotion:
+                    ValidateAmount(errors);
+                    break;
+                case EffectType.AddKey:
+                    ValidateAmount(errors);
+                    break;
+                case EffectType.RemoveKey:
+                    ValidateAmount(errors);
+                    break;
+
+                case EffectType.SetFlag:
+                    ValidateFlag(errors);
+                    break;
+                case EffectType.RemoveFlag:
+                    ValidateFlag(errors);
+                    break;
+
+                default:
+                    errors.Add("Type d'effet invalide.");
+                    break;
+            }
+
+            return errors.Count == 0;
+        }
+
+        private void ValidateAmount(List<string> errors)
+        {
+            if (Amount == null)
+            {
+                errors.Add($"{Type} : Amount est requis pour cet effet.");
+            }
+            else if (!ValidUtils.CheckIfPositiveNumber(Amount.Value))
+            {
+                errors.Add($"{Type} : Amount doit être un nombre positif (>= 1).");
+            }
+
+            if (FlagKey != null)
+            {
+                errors.Add($"{Type} : FlagKey doit être null pour cet effet.");
+            }
+        }
+
+        private void ValidateFlag(List<string> errors)
+        {
+            if (FlagKey == null)
+            {
+                errors.Add($"{Type} : FlagKey est requis pour cet effet.");
+            }
+
+            if (Amount != null)
+            {
+                errors.Add($"{Type} : Amount doit être null pour cet effet.");
+            }
+        }
+
+        private void ConsumeMany(int amount, Func<bool> consumeFunc)
+        {
+            for (int i = 0; i < amount; i++)
+            {
+                bool consumed = consumeFunc();
+                if (!consumed)
+                {
+                    break;
+                }
+            }
         }
 
     }
