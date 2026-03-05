64f2a31c31db0a7fb0dd52ff3d23f4872a3b307e
Kyo-XZ <ms.noctis@gmail.com>
2026-03-05 20:56:16 +0100

    feat : création du boutton test

--- View/MainPage.xaml.cs
+++ View/MainPage.xaml.cs
@@ -1,5 +1,13 @@
-﻿using ProjetPOO.Model.Gameplay;
-using ProjetPOO.Model.Inventory;
+﻿using ProjetPOO.Model.Combat;
+using ProjetPOO.Model.Gameplay;
+using ProjetPOO.Model.Story;
+using ProjetPOO.Model.Story.Enums;
+using Condition = ProjetPOO.Model.Story.Condition;
+using Effect = ProjetPOO.Model.Story.Effect;
+using ProjetPOO.Model.Combat.Enums;
+using ProjetPOO.Model.Data;
+using ProjetPOO.Model.Game;
+
 
 namespace ProjetPOO.View
 
@@ -14,15 +22,32 @@ namespace ProjetPOO.View
         private void buttonCreateLoot_Clicked(object sender, EventArgs e)
         {
             
-            Loot firstLoot = new Loot(1, 100, 2, 1);
-            Loot secondLoot = new Loot(2, 50, 1, 0);
+            Loot firstLoot = new Loot( 100, 2, 1);
+            Loot secondLoot = new Loot( 50, 1, 0);
 
         }
 
         private void buttonCreateInventory_Clicked(object sender, EventArgs e)
         {
-            Inventory firstInventory = new Inventory(1 , 5 , 5);
-            Inventory secondInventory = new Inventory(2, 3, 2);
+            Inventory firstInventory = new Inventory( 5 , 5);
+            Inventory secondInventory = new Inventory( 3, 2);
+        }
+
+        private void buttonInitialiseAllClass_Clicked(object sender, EventArgs e)
+        {
+            Choice choice = new Choice("Choix numéro 1", 0,0);
+            Scene scene = new Scene("Scène numéro 1", "Description de la scène numéro 1", SceneType.Normal);
+            Condition condition = new Condition(ConditionType.HasPotion, 1);
+            Effect effect = new Effect(EffectType.AddGold, 100);
+            Scenario scenario = new Scenario("Scénario numéro 1", "Description du scénario numéro 1");
+            PlayerCharacter player = new PlayerCharacter("Héros", 100, 20, 10, 5, 0, 1);
+            Enemy enemy = new Enemy("Gobelin 1", 50, 15, 5, 3, EnemyType.Goblin, 50, 0,25, 50,0,2,10,0,3);
+            CombatState combatState = new CombatState(player, enemy);
+            DataAccess dataAccess = new DataAccess();
+            Inventory inventory = new Inventory(5, 5);
+            Shop shop = new Shop("La boutique de Loïc", 10, 5);
+            Loot loot = new Loot(100, 2, 1);
+            GameState gameState = new GameState(1,1,10, inventory,player );
         }
     }
 
