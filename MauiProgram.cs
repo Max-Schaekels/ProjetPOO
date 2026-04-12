b517dde2011f2a44b62168a6e97be47bc002b280
Maximilien Schaekels <ms.noctis@gmail.com>
2026-04-12 15:37:05 +0200

    feat : ajout maui toolkit

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 using ProjetPOO.Utilities.DataAccess;
 using ProjetPOO.Utilities.DataAccess.Files;
 using ProjetPOO.Utilities.Interfaces;
@@ -18,6 +19,7 @@ namespace ProjetPOO
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
